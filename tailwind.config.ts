b7c98ac1484d70c97e195ad22df339d091b9cdb7
Shiv Satyam <84907785+demss233@users.noreply.github.com>
2025-08-08 03:14:24 +0530

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,6 +6,7 @@ const config: Config = {
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
+  
   theme: {
     extend: {
       backgroundImage: {
@@ -15,6 +16,8 @@ const config: Config = {
       },
     },
   },
+  
   plugins: [],
 };
+
 export default config;
